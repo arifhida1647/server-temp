83f31afddb52a0e43477b811038856a476450e06
arif hidayat <90981017+arifhida1647@users.noreply.github.com>
2024-02-16 07:25:21 +0700

    first commit

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,13 @@
-// server.js
-const express = require('express');
+const express = require("express");
+const router = express.Router();
 const axios = require('axios');
 
-const app = express();
+const routes = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+routes.use(express.json());
 
-app.get('/create-job', async (req, res) => {
+routes.get('/create-job', async (req, res) => {
   const { token, cron_expression, url } = req.query;
 
   try {
@@ -24,4 +24,6 @@ app.get('/create-job', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+module.exports = router;
